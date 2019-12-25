3270b2b9ab82b8884e98eb47a19b837fc2eeb17d
SameerBatla <sambatla456@gmail.com>
2019-12-25 23:53:59 +0530

    handling error on requests

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ mongoose.Promise = global.Promise;
 //middlewares
 app.use(express.json());
 app.use("/api", routes);
+app.use((err, req, res, next) => {
+  //   console.log(err);
+  res.status(422).send({
+    error: err.message
+  });
+});
 
 app.listen(process.env.port || 4000, () => {
   console.log("Now listening to request");
--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const Ninja = require("../models/ninja");
-router.get("/ninjas", (req, res) => {
+
+router.get("/ninjas", (req, res, next) => {
   res.send({
     type: "GET"
   });
 });
 
-router.post("/ninjas", (req, res) => {
+router.post("/ninjas", (req, res, next) => {
   console.log(req.body);
   //   var ninja = new Ninja(req.body)
   //   ninja.save()
-  Ninja.create(req.body).then(ninja => {
-    res.send(ninja);
-  });
+  Ninja.create(req.body)
+    .then(ninja => {
+      res.send(ninja);
+    })
+    .catch(next);
 });
 
-router.put("/ninjas/:id", (req, res) => {
+router.put("/ninjas/:id", (req, res, next) => {
   res.send({
     type: "PUT"
   });
 });
 
-router.delete("/ninjas/:id", (req, res) => {
+router.delete("/ninjas/:id", (req, res, next) => {
   res.send({
     type: "DELETE"
   });
